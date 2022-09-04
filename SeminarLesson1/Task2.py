010249d11dd201bb62a62aa2964a58b55446f527
GrafanyS <admin@valet.of.by>
2022-09-04 22:12:13 +0300

    Task2 две задачи

--- SeminarLesson1/Task2.py
+++ SeminarLesson1/Task2.py
@@ -1,6 +1,34 @@
 """
 1. Напишите программу для. проверки истинности утверждения
 ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+"""
+
+
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
+
+
+"""
 2. Напишите программу, которая принимает на вход координаты точки
 (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
 в которой находится эта точка (или на какой оси она находится).
