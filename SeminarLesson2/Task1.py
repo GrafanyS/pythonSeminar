c1596aad9b51099193d258ff1570429d2f0cb4df
GrafanyS <admin@valet.of.by>
2022-09-12 16:43:06 +0300

    seminarLessin add Tsak 1-4

---
+++ SeminarLesson2/Task1.py
@@ -0,0 +1,42 @@
+"""
+Урок 2. Знакомство с Python. Продолжение
+1 - Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр. Учтите, что числа могут быть отрицательными
+
+Пример:
+
+67.82 -> 23
+0.56 -> 11
+
+"""
+
+def input_number(text):
+
+    int_test = True
+    is_minus = False
+    while int_test:
+        coord = input(f"{text}")
+        if coord[0] == "-":
+            is_minus = True
+            coord = coord.replace("-", "")
+        if coord.isdigit():
+            coord = int(coord)
+            if is_minus:
+                coord *= -1
+            int_test = False
+        elif coord.isdecimal:
+            coord = float(coord)
+            if is_minus:
+                coord *= -1
+            int_test = False
+        else:
+            print("Not a number , try again")
+    return coord
+print('Используем программу, которая принимает на вход вещественное число и показывает сумму его цифр.')
+number = str(input_number("Введите число : "))
+
+result = 0
+for i in number:
+    if i.isdigit():
+        result = result+int(i)
+
+print(f"Сумма цифр введенного числа ({number}) равняется: {result}")
