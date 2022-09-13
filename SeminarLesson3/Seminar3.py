63e3dba49ac7790e1c0491751ecadaac3024098b
GrafanyS <admin@valet.of.by>
2022-09-13 19:51:55 +0300

    Create Semitar3

--- SeminarLesson3/Seminar3.py
+++ SeminarLesson3/Seminar3.py
@@ -24,7 +24,23 @@ print(''.join(list(filter(lambda char: char.isdigit(), num))))
 
 
 '''
-
+# list_of_str = ['апап4', 'fdgg3', 'fgdf', '6', 'fg24']
+#
+# number = int(input("Введите число: "))
+# position_of_number = -1
+# tem_position = -1
+# counter = 0
+# for elem in list_of_str:
+#     tem_position = elem.find(str(number))
+#
+#     if tem_position != -1:
+#         position_of_number = tem_position
+#         break
+#     counter += 1
+# if tem_position != -1:
+#     print(list_of_str,f"искали{number} ->", counter)
+# else:
+#     print(list_of_str, f"искали{number} ->а его нет!")
 
 '''
 2. Напишите программу, которая определит позицию второго вхождения строки в списке либо сообщит, что её нет.
@@ -38,9 +54,46 @@ print(''.join(list(filter(lambda char: char.isdigit(), num))))
 - список: [], ищем: "123", ответ: -1
 
 '''
+list_of_anytypes1 = ["qwe", "asd", "zxc",
+                     "qwe", "ertqwe"]  # , ищем: "qwe", ответ: 3
+list_of_anytypes2 = ["йцу", "фыв", "ячс", "цук",
+                     "йцукен", "йцу"]  # , ищем: "йцу", ответ: 5
+list_of_anytypes3 = ["йцу", "фыв", "ячс",
+                     "цук", "йцукен"]  # , ищем: "йцу", ответ: -1
+list_of_anytypes4 = ["123", "234", 123, "567"]  # , ищем: "123", ответ: -1
+list_of_anytypes5 = []  # , ищем: "123", ответ: -1
+
+user_sub_str = input("Введите строку: ")  # чисто для ошибки в переводе
+
+
+def find_my_substring(list_of_types, my_substring, list_of_str=None, number=None):
+    position_of_number = -1
+    tem_position = -1
+    counter = 0
+    for elem in list_of_types:
+        if (type(elem)) is str:
+            tem_position = elem.find(str(number))
+            if tem_position != -1:
+                position_of_number = tem_position
+                break
+        else:
+            continue
+        counter += 1
+
+    if tem_position != -1:
+        print(list_of_str, f"искали {number} -> его позиция", counter)
+    else:
+        print(list_of_str, f"искали {number} -> а его нет! ")
+
 
 
 '''
 3.Для натурального n создать словарь индекс-значение, состоящий из элементов последовательности 3n + 1.
 Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 '''
+# dictionary = {}
+# n=int(input('Введите число= '))
+# for i in range(1, n+1):
+#     dictionary[i]=3*i+1
+# print(dictionary)
+
